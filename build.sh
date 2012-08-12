eb529c7a8dc5bed2d1f99d731d86423a53843f93
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-12 11:39:49 -0400

    changed filter to include removing code blocks

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ R -e 'library(knitr);knit("report.Rmd")'
 # then pandoc to make markdown to LaTeX
 #pandoc -s -o report.tex report.md --template latex.template
 # this is a fudge to get the captions above the figures
-#  see addlabels.hsfor details
-pandoc -t json report.md | ./addlabels | pandoc -s -f json -t latex --template latex.template -o report.tex
+#  see myfilter.hs for details
+pandoc -t json report.md | ./myfilter | pandoc -s -f json -t latex --template latex.template -o report.tex
 
 # finally, compile the LaTeX
 xelatex report.tex
--- myfilter.hs
+++ myfilter.hs
@@ -1,9 +1,13 @@
--- A Pandoc filter to turn the "link text" into Figure labels
+-- Modified from:
+-- https://bitbucket.org/mpastell/pandoc-filters/src/6cd7b8522cf5/addlabels.hs
+--
+-- "A Pandoc filter to turn the "link text" into Figure labels
 -- in Latex output and parse `ref:xxx` into \ref{xxx} 
 -- Matti Pastell 2011 <matti.pastell@helsinki.fi> 
--- Requires Pandoc 1.8
--- https://bitbucket.org/mpastell/pandoc-filters/src/6cd7b8522cf5/addlabels.hs
-
+-- Requires Pandoc 1.8"
+--
+-- Now -- moves the caption above the figure
+--     -- removes Code formatting
 
 import Text.Pandoc
 import Text.Pandoc.Shared
@@ -16,13 +20,11 @@ addlabel (Para [Image xs (u, t)]) = RawBlock "latex"
           "\\begin{figure}[h]\n",
           "\\caption{" , (stringify xs) , "}\n" ,
           "\\includegraphics{" , u , "}\n" ,
---          "\\label{" , t ,  "}\n" ,
           "\\end{figure}"])
 addlabel x=x
 
 
 refs :: Inline -> Inline
 refs (Code attr code) 
-     | (take 4 code) == "ref:" = RawInline "tex" ("\\ref{" ++ (drop 4 code) ++ "}")
-     | otherwise = Code attr code
+     | otherwise = Str code
 refs x = x
