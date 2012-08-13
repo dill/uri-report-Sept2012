7ea56899a9feb37a697afab32b250993c32e0624
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-13 19:44:17 -0400

    actually a giant bug in evaluate :(

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ R -e 'library(knitr);knit("report.Rmd")'
 #pandoc -s -o report.tex report.md --template latex.template
 # this is a fudge to get the captions above the figures
 #  see myfilter.hs for details
-#pandoc -t json report.md | ./myfilter | pandoc -s -f json -t latex --template latex.template -o report.tex
-pandoc -t json report.md | pandoc -s -f json -t latex --template latex.template -o report.tex
+pandoc -t json report.md | ./myfilter | pandoc -s -f json -t latex --template latex.template -o report.tex
+#pandoc -t json report.md | pandoc -s -f json -t latex --template latex.template -o report.tex
 
 # finally, compile the LaTeX
 xelatex report.tex
